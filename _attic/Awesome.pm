151125ba2b180b7e23f0eae33b8eedef06e53e49
Kent Fredric <kentfredric@gmail.com>
2015-07-12 02:08:45 +1200

    Build results of 9eddbfb (on master)

---
+++ _attic/Awesome.pm
@@ -0,0 +1,15 @@
+use 5.006;    # our
+use strict;
+use warnings;
+
+package BeastForm::Awesome;
+
+our $VERSION = '0.000001';
+
+# AUTHORITY
+
+use Moo;
+with 'BeastForm::Role::Awesome';
+
+1;
+__END__
--- _attic/Col.pm
+++ _attic/Col.pm
@@ -6,7 +6,7 @@ package BeastForm::Col;
 
 our $VERSION = '0.000001';
 
-our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+# AUTHORITY
 
 use Moo;
 
@@ -52,30 +52,4 @@ sub new_from_table {
   );
 }
 1;
-
 __END__
-
-=pod
-
-=encoding UTF-8
-
-=head1 NAME
-
-BeastForm::Col
-
-=head1 VERSION
-
-version 0.000001
-
-=head1 AUTHOR
-
-James Laver <james.laver@gmail.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2015 by James Laver on time generously donated by Anomalio.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
